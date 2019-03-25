29dea50c5c6dc5a4b117d4da0313ab47ec201000
Vishal Kumar <vishal.kumar@jda.com>
2019-03-25 23:52:55 +0530

    Balanced Parenthesis with multiple brackets optimized solution

---
+++ test/src/recursion/BalancedParenthesis.java
@@ -0,0 +1,35 @@
+package recursion;
+
+public class BalancedParanthesis {
+
+	public static void main(String[] args) {
+		int n = 2;
+		char[] str = new char[n*2];
+		balancedParathesis(str, 0, 0, n, 0, 0);
+	}
+
+	public static void balancedParathesis (char[] str, int open, int close, int n, int pos, int prevPos) {
+		if(close == n ) {
+			for(int i=0; i < n*2; i++) {
+				System.out.print(str[i]);
+			}
+			System.out.println();
+			str = new char[n*2];
+		}
+			if(open < n) {
+				str[pos] = '(';
+				balancedParathesis(str, open+1, close, n, pos+1, pos+1);
+			}
+			if(open < n) {
+				str[pos] = '[';
+				balancedParathesis(str, open+1, close, n, pos+1, pos+1);
+			}
+			if(close < open) {
+				if(str[prevPos-1] == '(')
+					str[pos] = ')';
+				else if(str[prevPos-1] == '[')
+					str[pos] = ']';
+				balancedParathesis(str, open, close+1, n, pos+1, prevPos-1);
+			}
+	}
+}
